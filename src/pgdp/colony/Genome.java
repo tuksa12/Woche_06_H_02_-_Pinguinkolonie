7ad7c57b580d41cab7e1fba65b5604110846314e
DESKTOP-2KIND8H\Raphael Tuksa <rapha.tuksa@hotmail.com>
2020-12-17 20:17:47 +0100

    test 1

--- src/pgdp/colony/Genome.java
+++ src/pgdp/colony/Genome.java
@@ -44,6 +44,7 @@ public class Genome {
         dna = sb.toString();
     }
     public static Genome combine(Genome mother, Genome father){
+        String result;
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < LENGTH; i++) {
             if (randomBoolean(1000)){
@@ -53,6 +54,143 @@ public class Genome {
             }else{
                 sb.append(father.dna.charAt(i));
             }
+        }result = sb.toString();
+        return new Genome(result);
+    }
+    public String getGene(int genePos, int geneLength){
+        StringBuilder sb = new StringBuilder();
+        for (int i = genePos; i < geneLength; i++) {
+            sb.append(dna.charAt(genePos));
         }
-    return null;}
+        return sb.toString();
+    }
+    public static int interpretGene(String gene){
+        int result = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < LENGTH; i++) {
+            if (gene.charAt(i) == 'A'){
+                sb.append(0);
+            }else if (gene.charAt(i) == 'C'){
+                sb.append(1);
+            }else if (gene.charAt(i) == 'G'){
+                sb.append(2);
+            }else if (gene.charAt(i) == 'T'){
+                sb.append(3);
+            }
+        }
+        for (int i = 0; i < LENGTH; i++) {
+            result = result + (sb.codePointAt(i) * ((int) Math.pow(4,LENGTH-1-i)));
+        }
+        return result;
+    }
+    //Teilaufgabe 1.3: Gen-Interpretation
+    public int lifespan(){
+        int sum = 0;
+        int result;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 2; i++) {
+            if (dna.charAt(i) == 'A'){
+                sb.append(0);
+            }else if (dna.charAt(i) == 'C'){
+                sb.append(1);
+            }else if (dna.charAt(i) == 'G'){
+                sb.append(2);
+            }else if (dna.charAt(i) == 'T'){
+                sb.append(3);
+            }
+        }
+        for (int i = 0; i < 2; i++) {
+            sum = sum + (sb.codePointAt(i) * ((int) Math.pow(4,LENGTH-1-i)));
+        }
+        result = 8 + sum;
+        return result;
+    }
+
+    public int maxSize(){
+        int sum = 0;
+        int result;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i < 4; i++) {
+            if (dna.charAt(i) == 'A'){
+                sb.append(0);
+            }else if (dna.charAt(i) == 'C'){
+                sb.append(1);
+            }else if (dna.charAt(i) == 'G'){
+                sb.append(2);
+            }else if (dna.charAt(i) == 'T'){
+                sb.append(3);
+            }
+        }
+        for (int i = 2; i < 4; i++) {
+            sum = sum + (sb.codePointAt(i) * ((int) Math.pow(4,LENGTH-1-i)));
+        }
+        result = 16 + sum;
+        return result;
+    }
+
+    public int skill(){
+        int sum = 0;
+        int result;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 4; i < 9; i++) {
+            if (dna.charAt(i) == 'A'){
+                sb.append(0);
+            }else if (dna.charAt(i) == 'C'){
+                sb.append(1);
+            }else if (dna.charAt(i) == 'G'){
+                sb.append(2);
+            }else if (dna.charAt(i) == 'T'){
+                sb.append(3);
+            }
+        }
+        for (int i = 4; i < 9; i++) {
+            sum = sum + (sb.codePointAt(i) * ((int) Math.pow(4,LENGTH-1-i)));
+        }
+        result = sum;
+        return result;
+    }
+
+    public byte color(){
+        int sum = 0;
+        byte result;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 9; i < 12; i++) {
+            if (dna.charAt(i) == 'A'){
+                sb.append(0);
+            }else if (dna.charAt(i) == 'C'){
+                sb.append(1);
+            }else if (dna.charAt(i) == 'G'){
+                sb.append(2);
+            }else if (dna.charAt(i) == 'T'){
+                sb.append(3);
+            }
+        }
+        for (int i = 9; i < 12; i++) {
+            sum = sum + (sb.codePointAt(i) * ((int) Math.pow(4,LENGTH-1-i)));
+        }
+        result = (byte)sum;
+        return result;
+    }
+
+    public boolean isMale(){
+        boolean result;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 12; i < 13; i++) {
+            if (dna.charAt(i) == 'A'){
+                sb.append(0);
+            }else if (dna.charAt(i) == 'C'){
+                sb.append(1);
+            }else if (dna.charAt(i) == 'G'){
+                sb.append(2);
+            }else if (dna.charAt(i) == 'T'){
+                sb.append(3);
+            }
+        }
+        if (sb.codePointAt(0)==0 || sb.codePointAt(0)==2){
+            result = true;
+        }else{
+            result = false;}
+
+        return result;
+    }
 }
